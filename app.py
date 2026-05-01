871277403cd5867ec11f83cce4407f1f3d1c23db
Benjamin Haddad <couchroomkid@Benjamins-MacBook-Air-109.local>
2026-05-01 14:57:06 -0600

    fist commit

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template, send_from_directory
+from flask_cors import CORS
+from dotenv import load_dotenv
+load_dotenv()
+
+app = Flask(__name__, template_folder='public', static_folder='public/static')
+CORS(app)
+
